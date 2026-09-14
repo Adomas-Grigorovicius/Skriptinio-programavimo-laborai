a63cb6d64fd461e7109f52a692246c9cfe476d1c
Adomas <139012457+Adomas-Grigorovicius@users.noreply.github.com>
2026-09-14 13:11:57 +0300

    Update LD1_AdomasGrigorovicius.m

--- LAB1/LD1_AdomasGrigorovicius.m
+++ LAB1/LD1_AdomasGrigorovicius.m
@@ -11,3 +11,12 @@ ylabel('F_1 [-0-] | F_2 [-x-]')
 help linspace;
 help size;
 help max;
+
+N = 1;
+V = N+1:0.5:N+4;
+A = [N N+1 N+2; N+3 N+4 N+5; N+6 N+7 N+8];
+Aa = A(3,2);
+Ab = A(2:3,1:2);
+Ac = A([1 3],[1 3]);
+V2 = V(1:3);
+A2 = [A V2'];
