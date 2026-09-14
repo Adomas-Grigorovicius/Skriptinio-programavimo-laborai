a97f1d7a9c33a5388ddfb813f225e905372ca11a
Adomas <139012457+Adomas-Grigorovicius@users.noreply.github.com>
2026-09-14 12:52:11 +0300

    Update ld1_AdomasGrigorovicius.m

--- LAB1/ld1_AdomasGrigorovicius.m
+++ LAB1/ld1_AdomasGrigorovicius.m
@@ -1,2 +1,9 @@
-%Adomas Grigorovičius LD1 2026-09-14 EDif-25/1
+% Adomas Grigorovičius LD1 2026-09-14 EDif-25/1
 
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r',x,y/3,'xb')
+title('Dvi funkcijos')
+xlabel("X-ai")
+ylabel('F_1 [-0-] | F_2 [-x-]')
